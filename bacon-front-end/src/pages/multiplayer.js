40ae32fccead89e7a506a21b6f2e99fbdd7404cc
BVanCaster <bvancast@nd.edu>
2024-04-09 00:14:19 +0000

    introduced navbar components

---
+++ bacon-front-end/src/pages/multiplayer.js
@@ -0,0 +1,11 @@
+// pages/multiplayer.js
+import React from "react";
+
+const Multiplayer = () => {
+        return (
+                <div>
+                        <h1>Multiplayer Mode</h1>
+                </div>
+        );
+};
+export default Multiplayer;
---
+++ bacon-front-end/src/pages/singleplayer.js
@@ -0,0 +1,11 @@
+// pages/singleplayer.js
+import React from "react";
+
+const Singleplayer = () => {
+	return (
+		<>
+			<h1>Singleplayer Mode</h1>
+		</>
+	);
+};
+export default Singleplayer;
