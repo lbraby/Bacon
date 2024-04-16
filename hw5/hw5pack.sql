64917db9a9930e45b3842cf0b67858ecfc303f26
EC2 Default User <lbraby0314@gmail.com>
2024-04-16 23:27:12 +0000

    step 1 done

--- hw5/hw5pack.sql
+++ hw5/hw5pack.sql
@@ -1,7 +1,9 @@
 create or replace package hw5pack
 is
- function getcustname(id customer.cust_id%type)
+  function getcustname(id customer.cust_id%type)
     return customer.cust_name%type;
-  
+  function getsalpersname(id salesperson.salpers_id%type)
+    return salesperson.salpers_name%type;
+  procedure custlist (sid salesperson.salpers_id%type);
 end hw5pack;
 /
--- hw5/hw5packbody.sql
+++ hw5/hw5packbody.sql
@@ -12,8 +12,62 @@ is
     return name;
   exception
     when no_data_found then
-      dbms_output.put_line('getcustname eror: ' || id || ' not found');
+      dbms_output.put_line('getcustname error: ' || id || ' not found');
       return null;
   end;
+
+  function getsalpersname(id salesperson.salpers_id%type)
+    return salesperson.salpers_name%type
+  is
+    name salesperson.salpers_name%type;
+  begin
+    select salpers_name
+    into name
+    from salesperson
+    where salpers_id = id;
+    return name;
+  exception
+    when no_data_found then
+      dbms_output.put_line('getsalpersname error ' || id || ' not found');
+      return null;
+  end;
+
+  procedure custlist (sid salesperson.salpers_id%type)
+  is
+    cursor cust_c (id salesperson.salpers_id%type)
+    is
+      select distinct s.cust_id
+      from sale s
+      where s.salpers_id = id;
+    
+  cid customer.cust_id%type;
+  counter binary_integer := 0;
+
+  nothing exception;
+
+  begin
+    dbms_output.put_line('Salesperson: ' || getsalpersname(sid)); 
+    dbms_output.put_line('Customers:');
+
+    open cust_c(sid);
+    loop
+      fetch cust_c into cid;
+      exit when cust_c%notfound;
+      counter := counter + 1;
+      dbms_output.put_line('   ' || counter || '. ' || getcustname(cid));
+    end loop;
+    close cust_c;
+
+    if counter = 0 then
+      raise nothing;
+    end if;
+
+  exception
+    when nothing then
+      dbms_output.put_line('no customers found');
+    when no_data_found then
+      dbms_output.put_line('no such salesperson');
+  end;
+    
 end hw5pack;
 /
