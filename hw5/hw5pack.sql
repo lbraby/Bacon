01cc4171c3f4bbea0388687fb43d78cfd07a3cbe
EC2 Default User <lbraby0314@gmail.com>
2024-04-16 17:43:17 +0000

    hw5 group work

--- hw5/hw5pack.sql
+++ hw5/hw5pack.sql
@@ -1,5 +1,7 @@
 create or replace package hw5pack
 is
+ function getcustname(id customer.cust_id%type)
+    return customer.cust_name%type;
   
 end hw5pack;
 /
--- hw5/hw5packbody.sql
+++ hw5/hw5packbody.sql
@@ -1,5 +1,19 @@
 create or replace package body hw5pack
 is
-
+  function getcustname(id customer.cust_id%type)
+    return customer.cust_name%type
+  is
+    name customer.cust_name%type;
+  begin
+    select cust_name
+    into name
+    from customer
+    where cust_id = id;
+    return name;
+  exception
+    when no_data_found then
+      dbms_output.put_line('getcustname eror: ' || id || ' not found');
+      return null;
+  end;
 end hw5pack;
 /
