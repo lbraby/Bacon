2e63540e538bb2bd049b9c7cc2a1309e6098f5c8
DJ <dadams22@nd.edu>
2024-04-18 06:58:26 +0000

    debugged bryce code, added #3, and added test cases

--- hw5/hw5packbody.sql
+++ hw5/hw5packbody.sql
@@ -111,7 +111,33 @@ is
 	return null;
   end;
 
+
+  function getprodcost(id product.prod_id%type)
+    return product.cost%type
+  is
+    prc product.cost%type;
+  begin
+    select cost
+    into prc
+    from product
+    where prod_id = id;
+    return prc;
+  exception
+    when no_data_found then
+	dbms_output.put_line('getprodcost error ' || id || ' not found');
+	return null;
+  end;
+
   function addition(n1 float, n2 float)
+    return float
+  is
+    s float := 0.00;
+  begin
+    s := n1 + n2;
+    return s;
+  end;
+
+  function subtraction(n1 float, n2 float)
     return float
   is
     s float := 0.00;
@@ -131,9 +157,9 @@ is
 
   procedure custtransactions (cid customer.cust_id%type)
   is 
-    cursor sales (id customer.cust_id%type)
+    cursor sales_c (id customer.cust_id%type)
     is 
-      select distinct sale_id from sale where cid=id;
+      select distinct s.sale_id from sale s where s.cust_id=id;
 
   sid sale.sale_id%type;
   counter binary_integer := 0;
@@ -143,21 +169,66 @@ is
 
   begin
     dbms_output.put_line('Customer: ' || getcustname(cid));
-    dbms_output.put_line('Sales:');
+    dbms_output.put_line('Transactions:');
 
-    open sales(sid);
+    open sales_c(cid);
     loop
-      fetch sales into sid;
-      exit when sales%notfound;
+      fetch sales_c into sid;
+      exit when sales_c%notfound;
       counter := counter + 1;
       total := total + multiplication(getqty(sid), getprodprice(getprodid(sid)));
-      dbms_output.put_line('   ' || counter || '. ' || multiplication(getqty(sid), getprodprice(getprodid(sid))));
+      dbms_output.put_line('   ' || sid || '. ' || multiplication(getqty(sid), getprodprice(getprodid(sid))));
     end loop;
-    close sales;
+    close sales_c;
     dbms_output.put_line('Sum: ' || total);
     if counter = 0 then
       raise nothing;
     end if;
+
+  exception
+    when nothing then
+      dbms_output.put_line('no sales found');
+    when no_data_found then
+      dbms_output.put_line('no such customer');
+  end;
+  procedure mytransactions (sp_id salesperson.salpers_id%type)
+  is 
+    cursor sales_c (id salesperson.salpers_id%type)
+    is 
+      select distinct s.sale_id from sale s where s.salpers_id=id;
+
+  sid sale.sale_id%type;
+  counter binary_integer := 0;
+  total_rev float := 0.00;
+  total_cost float := 0.00;
+
+  nothing exception;
+
+  begin
+    dbms_output.put_line('Salesperson: ' || getsalpersname(sp_id));
+    dbms_output.put_line('Transactions:');
+
+    open sales_c(sp_id);
+    loop
+      fetch sales_c into sid;
+      exit when sales_c%notfound;
+      counter := counter + 1;
+      total_rev := total_rev + multiplication(getqty(sid), getprodprice(getprodid(sid)));
+      total_cost := total_cost + multiplication(getqty(sid), getprodcost(getprodid(sid)));
+      dbms_output.put_line('   ' || sid || '. ' || multiplication(getqty(sid), getprodprice(getprodid(sid))));
+    end loop;
+    close sales_c;
+    dbms_output.put_line('Total Cost: ' || total_rev);
+    dbms_output.put_line('Total Profit: ' || subtraction(total_rev, total_cost));
+    if counter = 0 then
+      raise nothing;
+    end if;
+
+  exception
+    when nothing then
+      dbms_output.put_line('no sales found');
+    when no_data_found then
+      dbms_output.put_line('no such salesperson');
   end;
 
 end hw5pack;
