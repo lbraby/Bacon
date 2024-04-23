a47f1ba76ede320c77e0cfdfa603b429dc7ffe41
EC2 Default User <lbraby0314@gmail.com>
2024-04-23 18:02:41 +0000

    load cast_and_crew data ignoring errors

--- bacon-back-end/build-movie-db/buildmoviedb.sh
+++ bacon-back-end/build-movie-db/buildmoviedb.sh
@@ -4,6 +4,6 @@ sqlplus -S guest/guest @create/crt_cast_and_crew
 sqlplus -S guest/guest @create/crt_dailymode
 sqlldr guest/guest control=control/movie.ctl
 sqlldr guest/guest control=control/person.ctl
-sqlldr guest/guest control=control/cast.ctl
-sqlldr guest/guest control=control/directors.ctl
+sqlldr guest/guest control=control/cast.ctl errors=9999999
+sqlldr guest/guest control=control/directors.ctl errors=9999999
 
