f0a80b74a02552ac66ae1618866534ea17a8f295
DJ <dadams22@nd.edu>
2024-04-24 16:03:23 +0000

    added flask_cors

--- bacon-back-end/API.py
+++ bacon-back-end/API.py
@@ -2,8 +2,10 @@ import json
 from flask import Flask, request, jsonify
 import cx_Oracle
 import random
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods=["GET"])
 def query_all():
