d1216a003acb0c3b4e7ebcb3eff297c37f2b9715
EC2 Default User <ec2-user@i-0a4cf09af642e43ce.aws.nd.edu>
2024-04-25 20:58:47 +0000

    TMDB data update

--- bacon-back-end/build-movie-db/buildmoviedb.sh
+++ bacon-back-end/build-movie-db/buildmoviedb.sh
@@ -2,8 +2,8 @@ sqlplus -S guest/guest @create/crt_movie
 sqlplus -S guest/guest @create/crt_person
 sqlplus -S guest/guest @create/crt_cast_and_crew
 sqlplus -S guest/guest @create/crt_dailymode
-sqlldr guest/guest control=control/movie.ctl
-sqlldr guest/guest control=control/person.ctl
+sqlldr guest/guest control=control/movie.ctl errors=9999999
+sqlldr guest/guest control=control/person.ctl errors=9999999
 sqlldr guest/guest control=control/cast.ctl errors=9999999
 sqlldr guest/guest control=control/directors.ctl errors=9999999
 
