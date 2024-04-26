94dc5d2d90bb9924068bc75318a20145f666d969
dadams2001 <dadams22@nd.edu>
2024-04-25 23:11:29 -0400

    data flow for singleplayer working

--- bacon-front-end/src/pages/singleplayer.js
+++ bacon-front-end/src/pages/singleplayer.js
@@ -2,73 +2,107 @@
 import React, {useEffect, useState}from "react";
 import "./singleplayer.css"
 const Singleplayer = () => {
-	const [flag, setFlag] = useState(true); //Flag for first time through
-	const [m2a, setM2a] = useState({}); // movie to actor result
-	const [m2m, setM2m] = useState({}); // movie to movie result
+	const [firstTimeDone, setFirstTimeDone] = useState(false); //Flag for first time through
 	const [dailyActors, setDailyActors] = useState({}); // 2 daily actors
 	const [searchVal, setSearchVal] = useState(""); // search bar value
 	const [searchData, setSearchData] = useState([]); // search bar stuff
-	const [selectedMovie, setSelectedMovie] = useState([]); // essentially current movie
+	const [selectedMovie, setSelectedMovie] = useState({}); // essentially current movie
 	const [boxDisplay, setBoxDisplay] = useState([]); // Info to be displayed
 
-	const appendToBoxDisplay = (thing)=> {
-		// Appends to the list to be displayed on the UI
-		let prevBoxDisplay = [...boxDisplay];
-		prevBoxDisplay.push(thing);
-		setBoxDisplay(prevBoxDisplay);
-	}
+	// const appendToBoxDisplay = (thing)=> {
+	// 	// Appends to the list to be displayed on the UI
+	// 	let prevBoxDisplay = [...boxDisplay];
+	// 	prevBoxDisplay.push(thing);
+	// 	setBoxDisplay(prevBoxDisplay);
+	// }
 
 	const movie2actor = (movieid, actorid) => {
 		// Calls movieperson API to compare an actor and movie
-		fetch(`http://172.22.132.192:8000/link/movieperson/${movieid}/${actorid}`)
+		return new Promise((resolve, reject) => {
+			fetch(`${process.env.REACT_APP_API_URL}/link/movieperson/${movieid}/${actorid}`)
 			.then((resp) => {
 				if(!resp.ok) {
-					throw new Error ("404")
+					reject(new Error ("404"));
 				} else {
 					return(resp.json());
 				}
 			})
-			.then(data => setM2a(data))
-			.catch(err => { console.error(err); });
+			.then(data => 
+			{
+				if (data.result === "success") {
+					resolve(1);
+				} else {
+					resolve(0);
+				}
+			})
+		})
 	}
 	
-	const movie2movie = (movie) => {
+	const movie2movie = (movie_id, selectedMovie_id) => {
 		// Searches moviemovie API for comparing two movies
-		fetch(`http://172.22.132.192:8000/link/moviemovie/${selectedMovie.movie_id}/${movie.movie_id}`)
+		return new Promise((resolve, reject) => {
+			fetch(`${process.env.REACT_APP_API_URL}/link/moviemovie/${selectedMovie_id}/${movie_id}`)
 			.then((resp) => {
 				if(!resp.ok) {
-					throw new Error ("404")
+					reject(new Error ("404"));
 				} else {
 					return(resp.json());
 				}
 			})
-			.then(data => console.log(data))
+			.then(data => {
+				if (data.result === "success") {
+					resolve(1);
+				} else {
+					resolve(0);
+				}
+			})
 			.catch(err => { console.error(err); });
+		});
 	}
 
 	
 	const movieSelected = (movie) => {
 		setSearchVal("");
-		console.log(movie);
-		if (flag) {
-
-			movie2actor(movie.movie_id, dailyActors.person1.person_id); //this is right for the previous click but not updating for the current one
-			// if m2a.result == 'success': set selectedMovie to movie, flag to false, append movie to boxDisplay
-			// if m2a.result == 'failure': return false
+		if (firstTimeDone) {
+			// if this is not the first time
+			movie2movie(movie.movie_id, selectedMovie.movie_id).then((result) => {
+				if (result) {
+					movie2actor(movie.movie_id, dailyActors.person2.person_id).then((result) => {
+						if (result) {
+							alert("he in both movies, game over");
+						} else {
+							alert("1st actor in movie, 2nd actor not tho, keep going!");
+						}
+					});
+					setSelectedMovie(movie);
+				} else {
+					alert("he not in that, try again");
+				}
+			});
 		} else {
-			movie2movie(movie.movie_id, selectedMovie.movie_id);
-			// if m2m isn't empty, it works
-			// if it works, add the movie and actor to boxDisplay, set selectedMovie to movie
-			// if it doesn't, return something false-esque
+			// if this is the first time
+			movie2actor(movie.movie_id, dailyActors.person1.person_id).then((result) => {
+				if (result) {
+					movie2actor(movie.movie_id, dailyActors.person2.person_id).then((result) => {
+						if (result) {
+							alert("he in both movies, game over");
+						} else {
+							alert("1st actor in the movie, 2nd actor not, keep going!");
+							setFirstTimeDone(true);
+						}
+					});
+					setSelectedMovie(movie);
+				} else {
+					alert("he not in that, try again");
+				}
+			});
 		}
-		movie2actor(selectedMovie.movie_id, dailyActors.person2.person_id);
-		// try movie2actor on the new selectedMovie and actor 2, if it works then end the game
-		// if it doesn't work, return true
 	}
 
 
 	useEffect(() => {
-		fetch(`http://172.22.132.192:8000/movies/search/${searchVal}/10`)
+		// call search api when search bar updated
+		fetch(`${process.env.REACT_APP_API_URL}/movies/search/${searchVal}/10`)
 				.then((resp) => {
 					if(!resp.ok) {
 						throw new Error ("we got ourselves a 404!")
@@ -84,7 +118,7 @@ const Singleplayer = () => {
 	},[searchVal]);
 
 	useEffect(() => {
-		fetch(`http://172.22.132.192:8000/dailymode`)
+		fetch(`${process.env.REACT_APP_API_URL}/dailymode`)
 			.then((resp) => {
 				if(!resp.ok) {
 					throw new Error ("404")
@@ -109,13 +143,13 @@ const Singleplayer = () => {
 					:
 					<div id="photos_box">
 						<div>
-							<img id="actor_photo1"/>
+							<img id="actor_photo1" alt="actor_photo1"/>
 							<div id="actor_name1">
 								<p>{dailyActors.person1.name}</p>
 							</div>
 						</div>
 						<div>
-							<img id="actor_photo2"/>
+							<img id="actor_photo2" alt="actor_photo1"/>
 							<div id="actor_name2"><p>{dailyActors.person2.name}</p></div>
 						</div>
 					</div>
@@ -126,9 +160,7 @@ const Singleplayer = () => {
 				<div style={{position: "absolute"}}>
 				{ (searchVal !== "") &&
 				<div style={{backgroundColor: '#E2E3E0', zIndex: 1, position: "relative"}}>
-					{console.log(searchData)}
 					{searchData.map((item, index) => {		
-						console.log(item);
 						return(
 							<p onClick={() => {movieSelected(item)}} key={index}>{item.title}</p>
 						);
@@ -137,6 +169,7 @@ const Singleplayer = () => {
 				</div>
 			</div>
 			<div id="actors_scroll">
+				{selectedMovie.title}
 				{boxDisplay.map(function(d, idx){
         				return (<li key={idx}>{d.name}</li>);
 				})}
